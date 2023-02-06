a1bd59e9ab8b7682e4dedaac4dc34aff858807ee
apostolossvls <apostolossvls@gmail.com>
2023-02-06 23:48:50 +0200

    Lesson 4

---
+++ TestEnviroment/Assets/Scripts/AgentPatrol.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentPatrol : MonoBehaviour
+{
+    public Transform[] targets;
+    int targetIndex;
+    NavMeshAgent agent;
+    public Animator animator;
+    float timer;
+    float waitTime;
+    float waitTimeMin = 6f;
+    float waitTimeMax = 7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        timer = 0;
+
+        targetIndex = -1;
+        GoNext();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance && !agent.pathPending)
+        {
+            animator.SetBool("Walking", false);
+
+            timer = timer + Time.deltaTime;
+
+            if (timer >= waitTime)
+            {
+                GoNext();
+            }
+        }
+        
+    }
+
+    void GoNext()
+    {
+        timer = 0;
+
+        waitTime = Random.Range(waitTimeMin, waitTimeMax);
+
+        targetIndex = (targetIndex + 1) % targets.Length;
+
+        animator.SetBool("Walking", true);
+        agent.SetDestination(targets[targetIndex].position);
+    }
+}
